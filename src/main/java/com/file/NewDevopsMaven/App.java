097475ca7998c4a51f0d259d80c2a531dccaa8aa
ramya <ramya@gmail.com>
2023-12-18 15:51:40 +0530

    first commit

---
+++ src/main/java/com/file/NewDevopsMaven/App.java
@@ -0,0 +1,13 @@
+package com.file.NewDevopsMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
